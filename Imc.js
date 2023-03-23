16bef78aa3162ad60e11935dba2430b1694428fe
Enstso509 <enstso@outlook.fr>
2023-03-23 16:11:04 +0100

    Update ReadMe

--- Imc.js
+++ Imc.js
@@ -3,6 +3,7 @@ var poids = document.getElementById('poids');
 var taille_value = 0;
 var poids_value = 0;
 var res=0;
+document.getElementById('txt-imc').classList.add('red');
 
 document.getElementById('button').addEventListener('click',function(event){
     event.preventDefault();
@@ -13,36 +14,36 @@ document.getElementById('button').addEventListener('click',function(event){
     document.getElementById('imc').innerText = "IMC : " + (res);
     
     if(res<18.5){
-        document.getElementById('txt-imc').classList.add('red');
+        document.getElementById('txt-imc').classList.replace('green','red')
         document.getElementById('txt-imc').innerText = 'Insuffisance pondérale (maigreur)';
         
     }
     else if (res>=18.5 && res<25) {
         document.getElementById('txt-imc').classList.replace('red','green')
-        document.getElementById('txt-imc').innerText = 'Corpulence normal';
+        document.getElementById('txt-imc').innerText = 'Corpulence normale';
 
     }
 
     else if(res>=25 && res<30){
-        document.getElementById('txt-imc').classList.add('red');
+        document.getElementById('txt-imc').classList.replace('green','red')
         document.getElementById('txt-imc').innerText = 'Surpoids';
 
     }
     
     else if( res>=30 && res<35){
-        document.getElementById('txt-imc').classList.add('red');
+        document.getElementById('txt-imc').classList.replace('green','red')
         document.getElementById('txt-imc').innerText = 'Obésité modérée';
 
     }
 
     else if(res>=35 && res < 40){
-        document.getElementById('txt-imc').classList.add('red');
+        document.getElementById('txt-imc').classList.replace('green','red')
         document.getElementById('txt-imc').innerText = 'Obésité sévère';
 
     }
 
     else{
-        document.getElementById('txt-imc').classList.add('red');
+        document.getElementById('txt-imc').classList.replace('green','red')
         document.getElementById('txt-imc').innerText = 'Obésité morbide';
 
     }
--- README.md
+++ README.md
@@ -4,11 +4,31 @@
 
 Dans l'apprentissage du JavaScript, l'objectif était d'utiliser les dom, pour manipuler les différents éléments de ma page HTML.
 
-Pour cela, j'ai coder un site web qui calcule L'imc d'une personne.
+Pour cela, j'ai codé un site web qui calcule L'imc d'une personne.
 
-![CalcultonImc.png](CalcultonImc.png)
+## Cas d'utilisation :
 
-Dans le cas où une personne mesure 170cm et pèse 86Kg :
+```plantuml
+@startuml Les bonnes pièces
+left to right direction
+:Utilisateur:
+package site{
+    Utilisateur--(Calculer son IMC)
+    
+}
+@enduml
+```
 
-![test.PNG](test.PNG)
+![CalcultonImc.png](Image/CalcultonImc.png)
 
+Dans le cas où une personne mesure 150 cm et pèse 60Kg :
+
+![test.PNG](Image/f1.PNG)
+
+Elle est en surpoids
+
+Dans le cas où une personne mesure 180 cm et pèse 70Kg :
+
+![test.PNG](Image/f2.PNG)
+
+Elle a une corpulence normale.
